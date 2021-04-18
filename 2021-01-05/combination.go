915afc30d773a260d89c8856d8bd81718da1fa7c
wyt2422762 <724752500@qq.com>
2021-04-18 18:37:59 +0800

    继承相关

--- 2021-01-05/combination.go
+++ 2021-01-05/combination.go
@@ -20,7 +20,7 @@ type child2 struct{
 }
 
 func main(){
-	/* var c1 child1 //定义一个子类
+	var c1 child1 //定义一个子类
 
 	fmt.Println(c1)
 
@@ -28,9 +28,21 @@ func main(){
 	c1.name = "子类"
 
 	c1.base.show()
-	c1.show() //该方法从父类继承，同 c.base.show() */
+	c1.show() //该方法从父类继承，同 c.base.show()
+
+	fmt.Println("-----------------------------")
+
+	var bs base
+	bs.age = 1
+	bs.name = "父类"
 
 	var c2 child2
+	fmt.Println(c2)
+	//c2.age = 2
+	//c2.name = "子类2"
+	c2.base = &bs
+	
+	c2.base.show()
 	c2.show()
 
 }
